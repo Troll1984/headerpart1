6bf4466f0c3e565338a9c5dacd3c2773d705b68c
Ortman Aleks <ortmanaleksej3@gmail.com>
2026-09-14 07:48:51 +0300

    save

--- src/js/open-menu.js
+++ src/js/open-menu.js
@@ -1,4 +1,4 @@
-let menu = document.querySelector('.menu');
+let menu = document.querySelector('#menu');
 let openMenu = document.querySelector('.open-menu');
 let btn = document.querySelector('.btn');
 
@@ -8,3 +8,22 @@ menu.addEventListener('click', function() {
 btn.addEventListener('click', function() {
      openMenu.style.display = "none"
 })
+
+
+/*Появленя блока контакт */
+let blockСontact = document.querySelector('.block-contact');
+let btnClose = document.querySelector('#btnclose'); 
+document.addEventListener('click', function() {
+   
+    setTimeout(() => {
+        blockСontact.style.display = 'flex';
+    }, 5000);
+   
+}, { once: true });
+btnClose.addEventListener('click', function() {
+     blockСontact.style.display = 'none';
+     setTimeout(() => {
+        blockСontact.style.display = 'flex';
+     },60000)
+});
+
