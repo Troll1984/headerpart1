f85cd643790cc180f242411710ca5fe9366277ca
Ortman Aleks <ortmanaleksej3@gmail.com>
2026-09-14 16:49:31 +0300

    save

--- src/js/tickets.js
+++ src/js/tickets.js
@@ -4,6 +4,7 @@ let modalWindow = document.querySelector('.modal-window');
 let overlay = document.querySelector('.overlay');
 let textPresent = document.querySelector('.text-present');
 let winSound = document.querySelector('#win-sound');
+let blockContact = document.querySelector('.block-contact');
 
 
 btnNumber.addEventListener('click', function() {
@@ -11,9 +12,14 @@ btnNumber.addEventListener('click', function() {
         overlay.classList.add('show');
         winSound.play();
          setTimeout(() => {
-            modalWindow.style.display = 'block';    
-                  
+            modalWindow.style.display = 'block';                 
         }, 3000)
+         setTimeout(() => {
+            modalWindow.classList.add('active');                
+        }, 7000)
+        setTimeout(() => {
+            modalWindow.classList.remove('active');                
+        }, 5000)
         setTimeout(() => {            
             textPresent.style.display = 'block';
         }, 4000)
@@ -27,6 +33,7 @@ modalWindow.addEventListener('click', function() {
     overlay.classList.remove('show');
     modalWindow.style.display = 'none';
     textPresent.style.display = 'none';
+    blockContact.style.display = 'none';
     winSound.pause();
-     winSound.currentTime = 0;
+    winSound.currentTime = 0;
 })
